56b77a1a9833e4d8327d9b45f8393189809c8057
runner <runner@Mac-1606230461509.local>
2020-11-24 15:13:44 +0000

    release/0.1.0

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "ZendeskSDKFayeClient",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "ZendeskSDKFayeClient",
+            targets: [
+                "ZendeskSDKFayeClientTargets"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(name: "ZendeskSDKSocketClient",
+                 url: "https://github.com/zendesk/sdk_socket_client_ios",
+                 from: "0.1.0"),
+        .package(name: "ZendeskSDKLogger",
+                 url: "https://github.com/zendesk/sdk_logger_ios",
+                 from: "0.1.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ZendeskSDKFayeClient",
+            path: "ZendeskSDKFayeClient.xcframework"
+        ),
+        .target(name: "ZendeskSDKFayeClientTargets",
+                dependencies: [
+                    .target(name: "ZendeskSDKFayeClient"),
+                    .product(name: "ZendeskSDKSocketClient", package: "ZendeskSDKSocketClient"),
+                    .product(name: "ZendeskSDKLogger", package: "ZendeskSDKLogger")
+        ])
+    ]
+)
