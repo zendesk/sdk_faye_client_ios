a5a51ef287080ac377539334909a74074502a995
runner <runner@Mac-1646147279188.local>
2022-03-01 15:30:00 +0000

    release/0.5.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(name: "ZendeskSDKSocketClient",
                  url: "https://github.com/zendesk/sdk_socket_client_ios",
-                 from: "0.4.3"),
+                 from: "0.5.0"),
         .package(name: "ZendeskSDKLogger",
                  url: "https://github.com/zendesk/sdk_logger_ios",
                  from: "0.4.3")
