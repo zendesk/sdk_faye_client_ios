47446c84a841fe3b7d38c4a57d631147c2ae0bdd
runner <runner@zendesk.com>
2025-07-30 12:46:34 +0000

    [RELEASE] New version 1.14.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(
             name: "ZendeskSDKSocketClient",
             url: "https://github.com/zendesk/sdk_socket_client_ios",
-            from: "1.12.0"
+            from: "1.13.0"
         ),
         .package(
             name: "ZendeskSDKLogger",
